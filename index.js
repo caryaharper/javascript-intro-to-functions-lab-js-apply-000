ac96fdae9c04da966cbc991edef4da5b03a78ad7
vivacious-queue-3174 <caryaharper@gmail.com>
2020-05-15 01:58:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma ( string ) {
-if (string.toLowerCase()) return "I can't hear you!";
+if (string.toLowerCase() === string) {
+  return "I can't hear you!";
+}
 }
 
